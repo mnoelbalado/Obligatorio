650364c2c4249fb130f1948e4cb0345f1b71ed23
Vangeloro <vangeloro@correo.um.edu.uy>
2025-06-02 18:20:17 -0300

    Delete src/main/java/Pelicula.java

--- src/main/java/Pelicula.java
+++
@@ -1,8 +0,0 @@
-public class Pelicula {
-    private String id;
-    private String titulo;
-    private double ingresos;
-    private LinkedList<string> generos;
-    private String idiomaOriginal;
-    
-}
