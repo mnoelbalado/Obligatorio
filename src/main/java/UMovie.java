272d3cd905990d3156c8c0d39750894a9e32c85d
Vangeloro <vangeloro@correo.um.edu.uy>
2025-06-05 08:51:55 -0300

    Empiezo trabjando en Umovie

--- src/main/java/UMovie.java
+++ src/main/java/UMovie.java
@@ -1,4 +1,10 @@
 
+
 public class UMovie {
+    //Variables para almacenar los datos
+    private MyLinkedList<String> peliculas;
+    private MyLinkedListt<String> creditos;
+    private MyLinkedList<String> ratings;
+
 
 }
