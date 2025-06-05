12bfff560ad5fd29665f724b0a42d3deea7bb9e0
Vangeloro <vangeloro@correo.um.edu.uy>
2025-06-05 09:00:14 -0300

    Mis cambios en Umovie

--- src/main/java/UMovie.java
+++ src/main/java/UMovie.java
@@ -3,7 +3,7 @@
 public class UMovie {
     //Variables para almacenar los datos
     private MyLinkedList<String> peliculas;
-    private MyLinkedListt<String> creditos;
+    private MyLinkedList<String> creditos;
     private MyLinkedList<String> ratings;
 
 
