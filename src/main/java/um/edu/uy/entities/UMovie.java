03ce8fc7a3985e6deda4bd3d616637645504ab9a
Vangeloro <vangeloro@correo.um.edu.uy>
2025-06-05 23:16:55 -0300

    hice el primer metodo

--- src/main/java/um/edu/uy/entities/UMovie.java
+++ src/main/java/um/edu/uy/entities/UMovie.java
@@ -1,45 +1,95 @@
 package um.edu.uy.entities;
 
+import um.edu.uy.tads.hashTable.MyHashTable;
 import um.edu.uy.tads.linkedList.MyLinkedList;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
 
-
-public class UMovie {
-    //Para almacenar los datos
+public class UMovie implements UmMovieint {
     private MyLinkedList<String> datos;
+    String Moviecsv = "resources/movies_metadata.csv";
+    String RatingCsv = "resources/ratings.csv";
 
-    //constructor
+
     public UMovie() {
         datos = new MyLinkedList<>();
+
+
     }
 
-    public void cargarDatos() {
-        try {
-            List<String> lines = Files.readAllLines(Paths.get("src/DATASETS v2/movies_metadata.csv"));
-            for (String line : lines) {
-                datos.add(line);
+
+    @Override
+    public void Top_5_peliculas_por_idioma(String idioma) throws IOException {
+        System.out.println("cargando datos");
+        MyLinkedList<Integer> idPeliculaS = new MyLinkedList<>();
+        CSVReader reader = new CSVReader(new FileReader(Moviecsv));
+
+        String[] fila;
+        reader.readNext(); // saltar encabezado
+
+        String idiomatabla = null;
+        int idPelicula = 0;
+
+        while ((fila = reader.readNext()) != null) {//Devuelve una lista con las columnas de las filas (elemento 1 primer columna y etc)
+            idiomatabla = fila[7];
+            idPelicula = Integer.parseInt(fila[5]);
+            System.out.println(idPelicula);
+            if (idiomatabla.equals(idioma.toLowerCase())) {//El lowercase lo que hace es convertir las entradas en minusucla ya que estan asi en la tabla
+                idPeliculaS.add(idPelicula);
             }
-            System.out.println("Datos cargados: " + datos.size());
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+
+
         }
+        reader.close();
+        reader = new CSVReader(new FileReader(RatingCsv));
+
+        float ratings = 0;
+        MyHashTable<Float, Integer>  mapa = new  MyHashTable<>(8); //Cambiar por heap
+        reader.readNext(); // saltar encabezado
+
+
+        while ((fila = reader.readNext()) != null) {//Devuelve una lista con las columnas de las filas (elemento 1 primer columna y etc)
+            idPelicula = Integer.parseInt(fila[1]);
+            ratings = Float.parseFloat(fila[2]) ;
+
+            mapa.put(ratings, idPelicula);
+
+
+        }
+        reader.close();
+
     }
 
-    //Metodo para cargar los datos
 
 
-    public static void main(String[] args) {
-        UMovie app = new UMovie();
-        app.cargarDatos();
+
+
+
+    @Override
+    public void Top_10_peliculas_mejor_calificacion_por_usuarios() {
+
+    }
+
+    @Override
+    public void Top_5_colecciones_mas_ingresos() {
+
+    }
+
+    @Override
+    public void Top_10_directores_mejor_calificacion() {
+
+    }
+
+    @Override
+    public void Actor_con_mas_calificaciones_en_cada_mes_del_anio(int anio) {
+
+    }
+
+    @Override
+    public void Usuario_con_mas_calificaciones_por_genero(String genero) {
+
     }
 }
-
-
-
-
-
-
