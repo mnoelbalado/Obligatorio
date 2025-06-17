439a76d8f572b6cb8faf2519f7a895e2295c8916
mnoelbalado <mnoelbalado@gmail.com>
2025-06-17 08:26:28 -0300

    se cambian los nombres a la notacion correcta de cleancode

--- src/main/java/um/edu/uy/entities/UmMovieint.java
+++ src/main/java/um/edu/uy/entities/UmMovieint.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 
 public interface UmMovieint {
     void cargarDatos();
-    void Top_5_peliculas_por_idioma(String idioma) throws IOException;
-    void Top_10_peliculas_mejor_calificacion_por_usuarios();
-    void Top_5_colecciones_mas_ingresos();
-    void Top_10_directores_mejor_calificacion();
-    void Actor_con_mas_calificaciones_en_cada_mes_del_anio(int anio);
-    void Usuario_con_mas_calificaciones_por_genero(String genero);
+    void Top5PeliculasPorIdioma(String idioma) throws IOException;
+    void Top10PeliculasMejorCalificacionPorUsuarios();
+    void Top5ColeccionesMasIngresos();
+    void Top10DirectoresMejorCalificacion();
+    void ActorConMasCalificacionesEnCadaMesDelAnio(int anio);
+    void UsuarioConMasCalificacionesPorGenero(String genero);
 
 
 }
