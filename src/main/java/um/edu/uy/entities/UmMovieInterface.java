442c57728887cd95b8e8bc1148a97c3338f1d9fd
mnoelbalado <mnoelbalado@gmail.com>
2025-06-22 19:27:18 -0300

    hago el tercer metodo. tuve que agregar al TAD hash el metodo keys. tuve que crear la clase auxiliar: coleccioInfo

--- src/main/java/um/edu/uy/entities/UmMovieInterface.java
+++ src/main/java/um/edu/uy/entities/UmMovieInterface.java
@@ -1,14 +1,22 @@
 package um.edu.uy.entities;
 
+import um.edu.uy.tads.linkedList.MyLinkedList;
+
 import java.io.IOException;
 
 public interface UmMovieInterface {
     void cargarDatos();
+
     void Top5PeliculasPorIdioma(String idioma) throws IOException;
+
     void Top10PeliculasMejorCalificacionPorUsuarios();
-    void Top5ColeccionesMasIngresos();
+
+    void Top5SagasMasExitosas(MyLinkedList<Pelicula> peliculas);
+
     void Top10DirectoresMejorCalificacion();
+
     void ActorConMasCalificacionesEnCadaMesDelAnio(int anio);
+
     void UsuarioConMasCalificacionesPorGenero(String genero);
 
 
